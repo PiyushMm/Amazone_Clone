6182bce649c7ecdcf470d3216e949ace968929a6
Piyush <103035026+8989830820@users.noreply.github.com>
2023-01-19 13:46:29 +0530

    Update Banner.jsx
    Change images to Images

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -7,7 +7,7 @@ import React from 'react'
 export default function Banner() {
   return (
     <div className='banner'>
-       <img src={'/images/banner_image.jpg'} alt="not showing" />
+       <img src={'/Images/banner_image.jpg'} alt="not showing" />
         </div>
   )
 }
